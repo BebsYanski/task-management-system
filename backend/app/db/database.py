115dc2d30139976be6dff460b5a4f846ebdb947a
BebsYanski <bebongnchuy@gmail.com>
2025-01-06 10:01:29 +0100

    changed database uri to corresponding database path in the server configuration file

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -7,7 +7,12 @@ db_port: int = 5432
 db_host: str = 'db'
 db_password: str = '21000'
 
-uri: str = F'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/task_db'
+# URI format: postgres://username:password@hostname:port/database_name?sslmode=require
+
+
+uri: str = F'postgres://avnadmin:AVNS_Ho7YPyhliX3KancEu9t@postgrestaskdb-task-management-systemv1.b.aivencloud.com:18629/defaultdb?sslmode=require'
+
+# uri: str = F'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/task_db'
 
 # Create the database engine
 engine = create_engine(uri)
